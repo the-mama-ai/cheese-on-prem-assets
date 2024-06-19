bcc8e2fc944e1634ada72a0b9f735ed72517db07
Hamza <hamza.gamouh@gmail.com>
2024-06-19 22:32:57 +0000

    CHEESE CLI : Small bug fixes

--- assets/install-cheese.sh
+++ assets/install-cheese.sh
@@ -17,33 +17,33 @@ ui_port=9003
 
 # Define global environment variables
 
-echo "export CHEESE_CUSTOMER=${customer}" >> ~/.bashrc 
-echo "export CHEESE_PASSWORD=${password}" >> ~/.bashrc 
-echo "export REPO_NAME='themamaai.azurecr.io/cheese'" >> ~/.bashrc 
-echo "export LICENSE_FILE='/data/cheese_license_file.json'" >> ~/.bashrc 
-echo "export CONFIG_FILE='/data/cheese_config_file.yaml'" >> ~/.bashrc 
-echo "export ASSETS_FOLDER='${HOME}/cheese-on-prem-assets/assets'" >> ~/.bashrc 
-echo "export LICENSING='True'" >> ~/.bashrc 
-echo "export IP=${ip}" >> ~/.bashrc 
-echo "export DB_PORT=${db_port}" >> ~/.bashrc 
-echo "export API_PORT=${api_port}" >> ~/.bashrc 
-echo "export UI_PORT=${ui_port}" >> ~/.bashrc 
+# echo "export CHEESE_CUSTOMER=${customer}" >> ~/.bashrc 
+# echo "export CHEESE_PASSWORD=${password}" >> ~/.bashrc 
+# echo "export REPO_NAME='themamaai.azurecr.io/cheese'" >> ~/.bashrc 
+# echo "export LICENSE_FILE='/data/cheese_license_file.json'" >> ~/.bashrc 
+# echo "export CONFIG_FILE='/data/cheese_config_file.yaml'" >> ~/.bashrc 
+# echo "export ASSETS_FOLDER='${HOME}/cheese-on-prem-assets/assets'" >> ~/.bashrc 
+# echo "export LICENSING='True'" >> ~/.bashrc 
+# echo "export IP=${ip}" >> ~/.bashrc 
+# echo "export DB_PORT=${db_port}" >> ~/.bashrc 
+# echo "export API_PORT=${api_port}" >> ~/.bashrc 
+# echo "export UI_PORT=${ui_port}" >> ~/.bashrc 
 
-REPO_NAME='themamaai.azurecr.io/cheese'
-FULL_IMAGE_NAME=$REPO_NAME"/cheese_inference:"$customer
-echo "export INFERENCE_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
+# REPO_NAME='themamaai.azurecr.io/cheese'
+# FULL_IMAGE_NAME=$REPO_NAME"/cheese_inference:"$customer
+# echo "export INFERENCE_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
 
-FULL_IMAGE_NAME=$REPO_NAME"/cheese-database:"$customer
-echo "export DB_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
+# FULL_IMAGE_NAME=$REPO_NAME"/cheese-database:"$customer
+# echo "export DB_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
 
-FULL_IMAGE_NAME=$REPO_NAME"/cheese-api:"$customer
-echo "export API_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
+# FULL_IMAGE_NAME=$REPO_NAME"/cheese-api:"$customer
+# echo "export API_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
 
-FULL_IMAGE_NAME=$REPO_NAME"/cheese-ui:"$customer
-echo "export UI_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
+# FULL_IMAGE_NAME=$REPO_NAME"/cheese-ui:"$customer
+# echo "export UI_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
 
-FULL_IMAGE_NAME=$REPO_NAME"/cheese-docs:"$customer
-echo "export DOCS_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
+# FULL_IMAGE_NAME=$REPO_NAME"/cheese-docs:"$customer
+# echo "export DOCS_IMAGE=${FULL_IMAGE_NAME}" >> ~/.bashrc 
 
 source ~/.bashrc
 sudo cp cheese /usr/local/bin
