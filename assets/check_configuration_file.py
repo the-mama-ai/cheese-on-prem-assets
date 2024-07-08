6ffd1dc7d95e327bd533dbabaf19faa56a46ae7e
hamzagamouh <hamza.gamouh@gmail.com>
2024-07-08 06:01:02 +0000

    CHEESE installation + repo organization

--- assets/check_configuration_file.py
+++
@@ -1,3 +0,0 @@
-import os
-
-assert os.path.exists(os.getenv("CONFIG_FILE")) , f"Configuration file {os.getenv('CONFIG_FILE')} doesn't exist"
--- assets/copy-files.sh
+++
@@ -1,7 +0,0 @@
-# !bin/sh
-INSTANCE_IP="X.X.X.X"
-ASSETS_DIR=""
-INPUT_FILE=""
-PRIVATE_KEY=""
-scp -i $PRIVATE_KEY $INPUT_FILE ubuntu@$INSTANCE_IP:/home/ubuntu/cheese
-scp -i $PRIVATE_KEY -r $ASSETS_DIR ubuntu@$INSTANCE_IP:/home/ubuntu/cheese
